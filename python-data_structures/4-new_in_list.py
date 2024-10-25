1aec8728761a1f5bde97a02753947a85c2486d12
ARIIK-ANTHONY <a.ariik@alustudent.com>
2024-10-25 02:12:28 -0700

    4. Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,14 +3,9 @@
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a list at a specific position."""
-    # If idx is negative or out of range, return a copy of the original list
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
 
-    # Create a copy of the original list
     new_list = my_list.copy()
-    
-    # Replace the element at the specified index
     new_list[idx] = element
-    
     return new_list
