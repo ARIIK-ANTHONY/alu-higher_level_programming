756bb9748e81e03874e2218611899d5aecf51aff
ARIIK-ANTHONY <a.ariik@alustudent.com>
2024-10-25 03:32:27 -0700

    10. Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-
 def divisible_by_2(my_list=[]):
-    """Check if numbers in a list are divisible by 2.
-
-    This function takes a list of integers and returns a new list 
-    where each element indicates whether the corresponding number 
-    in the original list is divisible by 2 (True) or not (False).
-    """
-    return [num % 2 == 0 for num in my_list]
+    return list(map(lambda x: True if x % 2 == 0 else False, my_list))
