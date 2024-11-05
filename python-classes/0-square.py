c8764e552c9404e5bd9ced6b09ba60555456bd42
ARIIK-ANTHONY <a.ariik@alustudent.com>
2024-11-05 21:25:25 +0200

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
 
-my_square = Square()
-print(type(my_square))  # Prints the type of the object
-print(my_square.__dict__)  # Prints the dictionary of attributes (should be empty)
+'''Defining a class Square.'''
+
+
+class Square:
+    '''Representing a square.'''
+    pass
