c579c54e9bfdaa43c70be9a9efad8f6dbc86ea8e
ARIIK-ANTHONY <a.ariik@alustudent.com>
2024-11-06 20:41:16 +0200

    7-base_geometry.py

--- python-inheritance/7-main.py
+++ python-inheritance/7-main.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+from base_geometry import BaseGeometry
 
 bg = BaseGeometry()
 
+# Test integer_validator without exceptions
 bg.integer_validator("my_int", 12)
 bg.integer_validator("width", 89)
 
+# Test integer_validator with exceptions
 try:
     bg.integer_validator("name", "John")
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    print(f"[{e.__class__.__name__}] {e}")
 
 try:
     bg.integer_validator("age", 0)
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    print(f"[{e.__class__.__name__}] {e}")
 
 try:
     bg.integer_validator("distance", -4)
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    print(f"[{e.__class__.__name__}] {e}")
+
+try:
+    bg.integer_validator("height", "four")
+except Exception as e:
+    print(f"[{e.__class__.__name__}] {e}")
+
+try:
+    bg.integer_validator("age", (4,))
+except Exception as e:
+    print(f"[{e.__class__.__name__}] {e}")
